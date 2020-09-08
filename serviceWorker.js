38ca766fd64ad49890284346a5148b319bacc376
Great-Emeka <greatestgreatemeka@gmail.com>
2020-09-08 21:46:37 +0100

    updated serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,24 +17,54 @@ const FILES_TO_CACHE = [
   '/cv/Chimezie-Emeka-Great-Resume.pdf',
   'images/about.png',
   '/'
-];
+]; //make an offline.html page and include here so it loads when no connection
+
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
+  // Precache static resources here.
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
 
   self.skipWaiting();
 });
 
+
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
+  // To Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+);
 
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-
+  // To Add fetch event handler here.
+  if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('/'); //You can make ofline.html page and put there
+                });
+          })
+  );
 });
